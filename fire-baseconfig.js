97840de7b40636a49cff8f1d278e33a7f3f6c581
jatin1808-web <jatinthakur2988@gmail.com>
2025-10-06 19:24:05 +0530

    Create fire-baseconfig.js

---
+++ fire-baseconfig.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "your-api-key-here",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "your-sender-id",
+    appId: "your-app-id"
+};
